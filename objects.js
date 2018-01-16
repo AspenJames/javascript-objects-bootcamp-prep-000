ef2679caa1ecabeec971955cb0c6eaa0e400eeea
Aspen James <aspenjames@tqca.org>
2018-01-16 01:13:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,14 @@
 var playlist = {
-  Anthony Green: "Young Legs"
+  Anthony Green: "Young Legs",
   Basement: "Aquasun"
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle
+  playlist.artistName = songTitle;
   return playlist
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName
+  delete playlist.artistName;
   return playlist
 }
